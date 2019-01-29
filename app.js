8272a96c1d6f889fc689f9c496e4ef4373d93eb9
sivacharantammana <sivacharantammana@wmg.com>
2019-01-29 19:09:19 +0530

    wow

--- app.js
+++ app.js
@@ -8,21 +8,33 @@ const couch = new NodeCouchDb({
         pass: '#infy123'
     }
 });
+const dbName = "customers";
+const viewurl = "_design/all_customers/_view/all";
 
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.get('/',function(req,res){
-    res.send("Working...fine SUPer");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get('/', function (req, res) {
+    res.render("index");
+    couch.get(dbName,viewurl).then(
+        function (data, headers, status) {
+            console.log(data.data.rows);
+            res.render("index", {
+                customers: data
+            });
+        },
+        function (err) {
+            res.send(err);
+        });
 });
 
-app.listen(3000,function(){
-console.log('Server started on port 3000');
+app.listen(3000, function () {
+    console.log('Server started on port 3000');
 });
-couch.listDatabases().then(function(dbs){
+couch.listDatabases().then(function (dbs) {
     console.log(dbs);
 });
