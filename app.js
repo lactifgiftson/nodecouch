ee25b6513630647430ae821627efb06460e73937
sivacharantammana <sivacharantammana@wmg.com>
2019-02-22 14:17:41 +0530

    ok

--- app.js
+++ app.js
@@ -12,17 +12,32 @@ const couch = new NodeCouchDb({
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.get('/',function(req,res){
-    res.send("Working...fine SUPer");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const dbName = "address";
+const viewUrl = "_design/all_customers/_view/new-view";
+
+app.get('/', function (req, res) {
+    couch.get(dbName, viewUrl).then(
+        function (data, headers, status) {
+            console.log(data.data.rows);
+            res.render('index', {
+                address: data.data.rows
+            });
+        }, function (err) {
+            res.send(err);
+        });
 });
 
-app.listen(3000,function(){
-console.log('Server started on port 3000');
+app.listen(3000, function () {
+    console.log('Server started on port 3000');
 });
-couch.listDatabases().then(function(dbs){
+couch.listDatabases().then(function (dbs) {
     console.log(dbs);
 });
+
+
+
